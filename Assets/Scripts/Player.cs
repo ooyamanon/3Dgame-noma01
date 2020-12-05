362f1f3a6544b3bfd073cce83144060a7131915f
pb8vvs38zx <pb8vvs38zx@privaterelay.appleid.com>
2020-12-05 16:12:28 +0900

    プロジェクト作成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3.0f;
+    float moveX = 0f;
+    float moveZ = 0f;
+
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * speed;
+        moveZ = Input.GetAxis("Vertical") * speed;
+        Vector3 direction = new Vector3(moveX, 0, moveZ);
+
+        controller.SimpleMove(direction);
+    }
+}
